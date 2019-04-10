7be3e97f1502eb2b90dea495d519d3bf5df38447
Salman-Moin <46302625+Salman-Moin@users.noreply.github.com>
2019-04-10 13:40:44 +0500

    objects in the array and calling them from map
    objects in the array and calling them from map

--- App.js
+++ App.js
@@ -1,78 +1,42 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const User = (props) => {
-  return(<div>Name: {props.children} | Age: {props.age}
-  </div>)
-}
+const list = [
+  {
+    title: "React",
+    url: "https://react.org",
+    author: "Jordon Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan, Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App"><Users/></div>
-    )
+      <div className="App">
+        {list.map(function(item) {
+          return <div>{item.title}
+                      {item.url}
+                      {item.author}
+                      {item.num_comments}
+                      {item.points}
+                      {item.objectID}
+                 </div>  
+        })} 
+      </div>
+    );
   }
-}
-
-class Users extends Component{
-  state = {
-    users: [
-      {name: "John", age: 20},
-      {name: "Jill", age: 30},
-      {name: "Peter", age: 40}
-    ],
-    title: "Users List"
-  };
-
-  // makeMeYounger = () => {
-  //   this.setState({
-  //     users: [
-  //       {name: "John", age: 10},
-  //       {name: "Jill", age: 20},
-  //       {name: "Peter", age: 30}
-  //     ]    
-  //   })
-  // }
-
-  makeMeYounger = () => {
-    const newState = this.state.users.map((user) => {
-      const tempUser = user;
-      tempUser.age -= 10;
-      return tempUser;
-    });
-    this.setState({
-      newState
-    });
-  }
-  
-  changeTitle = () => {
-    flag = true
-    const newTitle = "Users List - New Title!"
-    let flag
-    if (flag = true) 
-      this.setState({title: newTitle})
-   // else this.setState({title:title})
-    //this.state.title = this.state.newTitle
-  }
-
-  render(){
-    return(<div>
-      <button onClick={this.makeMeYounger}>Make Us 10 Years Younger</button>
-      {/* <input type="text" name="title"></input> */}
-      <button onClick={this.changeTitle}>Change Title</button>
-      <br/>
-      <h1>{this.state.title}</h1>
-      {
-        this.state.users.map((user) => {
-          return <User age={user.age}>{user.name}</User>
-        })
-      }
-
-      {/* <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
-      <User age={this.state.users[1].age}>{this.state.users[1].name}</User>
-      <User age={this.state.users[2].age}>{this.state.users[2].name}</User> */}
-    </div>)
-  }
-
 }
 
 export default App;
